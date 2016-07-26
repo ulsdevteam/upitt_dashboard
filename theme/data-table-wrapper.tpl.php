49fe0bfe4099851be7fcc7a889d9d77fd5df489d
Brian Gillingham <bgilling@pitt.edu>
2016-07-26 09:56:34 -0400

    renamed template, finished csv download

---
+++ theme/data-table-wrapper.tpl.php
@@ -0,0 +1,16 @@
+<?php
+//  print '<pre>';
+//  var_dump(get_defined_vars()); 
+//  print '</pre>';
+?>
+  <div class="item-list" style="font-size:smaller">
+    <ul class="links">
+      <li><?php print number_format(count($rows)) . ' record' . (count($rows) == 1 ? '' : 's'); ?></li>
+    <?php if ($download_csv) : ?>
+      <li><?php print $download_csv; ?></li>
+    <?php endif; ?>
+    <?php if ($search_link) : ?>
+      <li><?php print $search_link; ?></li>
+    <?php endif; ?>
+    </ul>
+  </div>
