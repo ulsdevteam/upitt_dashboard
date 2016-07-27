4fd9f38e7fea23d458b5ddf729d8f034e3c1898d
Brian Gillingham <bgilling@pitt.edu>
2016-07-27 11:25:05 -0400

    added view all link, new report added

--- theme/data-table-wrapper.tpl.php
+++ theme/data-table-wrapper.tpl.php
@@ -1,18 +1,20 @@
 <?php
 //  print '<pre>';
-//  var_dump(get_defined_vars()); 
+// var_dump(get_defined_vars()); 
 //  print '</pre>';
 ?>
   <div class="item-list" style="font-size:smaller">
     <ul class="links">
-      <li><?php print number_format(count($rows)) . ' record' . (count($rows) == 1 ? '' : 's'); ?></li>
+      <li><?php print number_format($num_found) . ' record' . ($num_found == 1 ? '' : 's'); ?></li>
     <?php if ($download_csv) : ?>
       <li><?php print $download_csv; ?></li>
+    <?php endif; ?>
+    <?php if ($view_all) : ?>
+      <li><?php print $view_all; ?></li>
     <?php endif; ?>
     <?php if ($search_link) : ?>
       <li><?php print $search_link; ?></li>
     <?php endif; ?>
     </ul>
   </div>
-  <div><?php echo $table;?></div>
-  <div><?php echo $paging;?></div>
+  <div style="overflow-x:auto"><?php print $table;?></div>
